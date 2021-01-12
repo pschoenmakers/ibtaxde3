ab9e23227cc09871164d2d8819471bddaa853a92
Eric Hoh <limit64@web.de>
2021-01-12 15:09:03 +0100

    fix ib added a lower case letter to symbols indicating non-us countries add some logging rename "nominal" to "quantity" add functions to group and calculate profit for "close first" order

--- ibtaxde3/ibtaxde3.py
+++ ibtaxde3/ibtaxde3.py
@@ -37,18 +37,23 @@ def get_data(filepaths):
 
 
 def group_trades(trades):
+    logger.info("grouping trades")
     groups = []
 
     trades_by_symbol = dict()
     for trade in trades:
         trade.open_quantity = trade.quantity
 
-        # ib added a lower case letter to some foreign securities' symbols
-        symbol = trade.symbol if trade.symbol[-1].isupper() else trade.symbol[:-1]
+        if trade.symbol[-1].islower():  # ib added a lower case letter to some foreign securities' symbols
+            symbol = trade.symbol[:-1]
+            logger.warning(f"ignoring lower case suffix to symbol: {trade.symbol} -> {symbol}")
+        else:
+            symbol = trade.symbol
 
         trades_by_symbol[symbol] = trades_by_symbol.get(symbol, []) + [trade]
 
     for symbol in trades_by_symbol:
+        logging.info(f"found {len(trades_by_symbol[symbol])} trades for symbol {symbol}")
         groups.extend(_group_trades_symbol(trades_by_symbol[symbol]))
 
     return groups
@@ -69,7 +74,7 @@ def _group_trades_symbol(trades):
 
         for group in groups:
             group.try_close(trade)
-            if trade.open_quantity == 0:  # try_close reduced open_nominal if position was fully or partially closed
+            if trade.open_quantity == 0:  # try_close reduced open_quantity if position was fully or partially closed
                 break
         else:  # the not closed nominal of the trade is treated as an opening trade -> new group
             group = TransactionGroup(trade)
@@ -78,7 +83,10 @@ def _group_trades_symbol(trades):
     return groups
 
 
-def calc_profit(groups, tax_year=None):
+def calc_profit(groups, tax_year=None):  # TODO implement tax_year handling
+    if not isinstance(groups[0], TransactionGroup):
+        return _calc_profit_inverted(groups, tax_year)
+
     for group in groups:
         ota = group.opening_transaction
         if ota.category == "buy":
@@ -99,6 +107,28 @@ def calc_profit(groups, tax_year=None):
                                   + cti.closed_quantity / abs(ota.quantity) * ota.fee_eur)
 
 
+def _calc_profit_inverted(inverted_groups, tax_year):  # TODO implement tax_year handling
+    for group in inverted_groups:
+        cta = group.closing_transaction
+
+        if cta.category == "buy":
+            cta.profit_eur = cta.value_eur + cta.fee_eur
+            for oti in group.opening_transaction_items:
+                ta = oti.transaction
+                oti.profit_eur = (oti.quantity / abs(ta.quantity)) * (ta.value_eur + ta.fee_eur)
+
+        else:
+            assert cta.category == "sell"
+            cta.profit_eur = Decimal(0)
+
+            for oti in group.opening_transaction_items:
+                ta = oti.transaction
+                cta.profit_eur += (oti.quantity * (cta.price_eur - oti.transaction.price_eur)
+                                   + oti.quantity / abs(ta.quantity) * ta.fee_eur
+                                   + oti.quantity / abs(ta.quantity) * cta.fee_eur)
+                oti.profit_eur = Decimal(0)
+
+
 def export_groups(filepath, groups):
     with open(filepath, "w") as f:
         writer = csv.writer(f, delimiter=";", quotechar='"')
@@ -116,8 +146,72 @@ def export_groups(filepath, groups):
             writer.writerow(row)
             for cti in group.closing_transaction_items:
                 c = cti.transaction
-                row = ["CLOSE", abs(cti.closed_quantity), c.transaction_id, c.date, c.settlement_date, c.category,
+                row = ["CLOSE", abs(cti.quantity), c.transaction_id, c.date, c.settlement_date, c.category,
                        c.currency, c.exchange_rate, c.asset_category, c.symbol, c.isin, c.description,
                        c.quantity, c.price, c.value, c.tax, c.fee, c.value_eur, c.tax_eur, c.fee_eur, cti.profit_eur]
                 writer.writerow(row)
             writer.writerow([])
+
+
+def export_groups_close_first(filepath, inverted_groups, groups=None):
+    """
+    export inverted groups (closing transaction first) into a csv file. If open short transaction should be
+    added groups parameter must be given.
+    """
+    with open(filepath, "w") as f:
+        writer = csv.writer(f, delimiter=";", quotechar='"')
+
+        row = ["OPEN/CLOSE", "closed quantity", "id", "date", "valuta", "buy/sell", "currency", "exchange rate",
+               "asset category", "symbol", "isin", "description", "quantity", "price", "value", "tax",
+               "fee", "value (EUR)", "tax (EUR)", "fee (EUR)", "profit (EUR)"]
+        writer.writerow(row)
+        
+        for group in inverted_groups:
+            c = group.closing_transaction
+            row = ["CLOSE", "", c.transaction_id, c.date, c.settlement_date, c.category, c.currency, c.exchange_rate,
+                   c.asset_category, c.symbol, c.isin, c.description, c.quantity, c.price, c.value, c.tax,
+                   c.fee, c.value_eur, c.tax_eur, c.fee_eur, c.profit_eur]
+            writer.writerow(row)
+
+            for oti in group.opening_transaction_items:
+                o = oti.transaction
+                row = ["OPEN", abs(oti.quantity), o.transaction_id, o.date, o.settlement_date, o.category,
+                       o.currency, o.exchange_rate, o.asset_category, o.symbol, o.isin, o.description,
+                       o.quantity, o.price, o.value, o.tax, o.fee, o.value_eur, o.tax_eur, o.fee_eur, oti.profit_eur]
+                writer.writerow(row)
+            writer.writerow([])
+
+        if groups is None:
+            return
+
+        for group in groups:
+            if group.opening_transaction.category == "sell" and not group.closing_transaction_items:
+                o = group.opening_transaction
+                row = ["OPEN", "", o.transaction_id, o.date, o.settlement_date, o.category, o.currency,
+                       o.exchange_rate, o.asset_category, o.symbol, o.isin, o.description, o.quantity, o.price,
+                       o.value, o.tax, o.fee, o.value_eur, o.tax_eur, o.fee_eur, o.profit_eur]
+                writer.writerow(row)
+
+
+def _get_groups_close_first(groups):
+    inverted_groups = []
+
+    InvertedGroup = namedtuple("InvertedGroup", "closing_transaction opening_transaction_items")
+
+    class _OpeningTransactionItem(object):
+        def __init__(self, transaction, quantity):
+            self.transaction = transaction
+            self.quantity = quantity
+            self.profit_eur = None
+
+    for group in groups:
+        for cti in group.closing_transaction_items:
+            oti = _OpeningTransactionItem(group.opening_transaction, cti.closed_quantity)
+            ig = [ig for ig in inverted_groups if ig.closing_transaction == cti.transaction]
+            if not ig:
+                ig = InvertedGroup(cti.transaction, [oti])
+                inverted_groups.append(ig)
+            else:
+                ig[0].opening_transaction_items.append(oti)
+
+    return inverted_groups
--- test/test_group_trades.py
+++ test/test_group_trades.py
@@ -8,10 +8,13 @@ class TestGroupTrades(unittest.TestCase):
         temp = ibtaxde3.get_data(["resource/2018.xml", "resource/2019.xml", "resource/2020.xml"])
         trades = temp.trades
 
-        result = ibtaxde3.group_trades(trades)
-        ibtaxde3.calc_profit(result)
+        groups = ibtaxde3.group_trades(trades)
+        ibtaxde3.calc_profit(groups)
+        # ibtaxde3.export_groups("groups.csv", result)
 
-        ibtaxde3.export_groups("groups.csv", result)
+        inverted_groups = ibtaxde3._get_groups_close_first(groups)
+        ibtaxde3.calc_profit(inverted_groups)
+        ibtaxde3.export_groups_close_first("inverted_groups.csv", inverted_groups, groups)
 
 
 if __name__ == '__main__':
